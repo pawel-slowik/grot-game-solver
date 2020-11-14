8fb568b75863969c0c276364d6593f66ac5b2efc
Paweł Słowik <pawel.slowik@zoho.com>
2020-11-14 12:23:00 +0100

    PEP8 whitespace fixes

--- grot.py
+++ grot.py
@@ -2,25 +2,32 @@
 
 from typing import NamedTuple, Sequence, Mapping
 
+
 class Position(NamedTuple):
     row: int
     column: int
 
+
 Path = Sequence[Position]
+
 
 class Move(NamedTuple):
     vertical: int
     horizontal: int
 
+
 Board = Mapping[Position, str]
+
 
 def find_best_path(board: Board) -> Path:
     all_paths = [travel(board, position) for position in board.keys()]
     return sorted(all_paths, key=path_weight)[-1]
 
+
 def path_weight(path: Path) -> int:
     # skip already visited fields when choosing the best path
     return len(set(path))
+
 
 def travel(board: Board, start_position: Position) -> Path:
     position = start_position
@@ -33,12 +40,14 @@ def travel(board: Board, start_position: Position) -> Path:
         position = new_position
     return path
 
+
 def move(board: Board, position: Position, path: Path) -> Position:
     delta = compute_next_move(board, position, path[:-1])
     return Position(
         row=position.row + delta.vertical,
         column=position.column + delta.horizontal,
     )
+
 
 def compute_next_move(board: Board, position: Position, path: Path) -> Move:
     direction_map = {
@@ -56,6 +65,7 @@ def compute_next_move(board: Board, position: Position, path: Path) -> Move:
         )
     return direction_map[board[position]]
 
+
 def matrix_to_board(matrix: Sequence[Sequence[str]]) -> Board:
     board = {}
     for row_number, row in enumerate(matrix):
@@ -63,14 +73,16 @@ def matrix_to_board(matrix: Sequence[Sequence[str]]) -> Board:
             board[Position(row=row_number, column=column_number)] = direction
     return board
 
+
 def main() -> None:
     sample = [
-        ['u', 'd', 'u', 'u'], # ↑ ↓ ↑ ↑
-        ['u', 'r', 'l', 'l'], # ↑ → ← ←
-        ['u', 'u', 'l', 'u'], # ↑ ↑ ← ↑
-        ['l', 'd', 'u', 'l'], # ← ↓ ↑ ←
+        ['u', 'd', 'u', 'u'],  # ↑ ↓ ↑ ↑
+        ['u', 'r', 'l', 'l'],  # ↑ → ← ←
+        ['u', 'u', 'l', 'u'],  # ↑ ↑ ← ↑
+        ['l', 'd', 'u', 'l'],  # ← ↓ ↑ ←
     ]
     print(find_best_path(matrix_to_board(sample))[0])
+
 
 if __name__ == "__main__":
     main()
